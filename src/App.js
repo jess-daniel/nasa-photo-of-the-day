620641a47e9fe0788d98bd0ca4d8900b2632d9e8
Jess Daniel <jessdaniel474@gmail.com>
2019-10-09 15:40:37 -0400

    Made API call and added data to state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect, } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(response => {
+        console.log(response.data);
+        setNasaData(response.data);
+      })
+      .catch(error => {
+        console.log("Data not returned: ", error);
+      })
+  }, []);
+
   return (
     <div className="App">
       <p>
